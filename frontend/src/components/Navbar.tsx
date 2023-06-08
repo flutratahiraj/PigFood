d637fd5e692879caeea41df0364fbea3476d7159
kzethraeus <zethraeusklara@gmail.com>
2023-06-08 17:31:45 +0200

    Changed to big L in Logo-navbar to match and se if logo can be visible in our backend render.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-// import HeaderMobile from "./HeaderMobile";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
     <>
       <div className="Navbar-container">
-        <img src="logo-navbar.png" alt="Logo" className="navbar-logo" />
-        {/* <HeaderMobile
-          joinButtonProps={{
-            label: "Join the pigsty!",
-            onClick: () => {
-              // Hantera onClick-händelsen
-            },
-          }}
-        /> */}
+        <img src="/Logo-navbar.png" alt="Logo" className="navbar-logo" />
       </div>
     </>
   );

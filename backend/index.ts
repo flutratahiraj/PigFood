a2e51fc6fe4baefefdaca05f82ce093c76bc2b05
LingdenHem <hemcoolgamer1@gmail.com>
2023-06-09 21:12:08 +0200

    added feedback in backend so it can be fetched using axios

--- backend/index.ts
+++ backend/index.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 import dotenv from "dotenv";
 import { Client } from "pg";
-// import LocalClient from "./conection";
+import LocalClient from "./conection";
 import accountRouter from "./accountRoter";
 const app = express();
 
@@ -23,12 +23,44 @@ export const client = new Client({
 });
 
 //min lokala anslutning
-// LocalClient.connect();
+LocalClient.connect();
 //från handou: "publicering via Heroku"
 //varför är det "UNDERSCORE_request"
 app.get("/api", (_request, response) => {
   response.send({ hello: "World" });
 });
+
+app.get("/feedback", async (request, response) => {
+
+  response.send([
+
+    {
+      name: 'Lisa McFlanagan',
+      image: 'AVATAR.png',
+      description:
+        'This food website is the true gem in the gastronomic universe! The dishes here are so mouthwatering, I can practically taste them through my screen. I\'ve become a self-proclaimed foodie ninja, effortlessly navigating through the menu, unleashing my inner epicurean. Warning: side effects may include sudden cravings, uncontrollable foodgasms, and an addiction to refreshing the page for new culinary adventures',
+      nationality: 'Probably Irish'
+    },
+    {
+      name: 'Bubba',
+      image: 'Bubba.svg',
+      description:
+        '“Wowza! This food website is like a flavor explosion in my mouth, and my taste buds are doing the happy dance! I use it all the time. It feels great to contribute to a better global environment and for my economy.”',
+      nationality: 'Probably Irish',
+    },
+    {
+      name: 'Seb Murphy',
+      image: 'AVATAR.png',
+      description:
+        '“This site makes me fat and happy.”',
+      nationality: 'Probably Irish',
+    },
+  ]
+);
+
+
+});
+
 
 // // Tidigare kod
 // //göra images mappen tillgänglig via http://localhost/images
@@ -36,7 +68,7 @@ app.use("/images", express.static("images"));
 
 // // Tidigare kod,
 // //men borde nog ingå även via publicering...
-client.connect();
+// client.connect();
 
 // app.get("/cities", (request, response) => {
 //   client.query("SELECT * FROM cities", (error, result) => {
@@ -48,6 +80,9 @@ client.connect();
 //från handou: "publicering via Heroku"
 app.use(express.static(path.join(path.resolve(), "public")));
 const port = process.env.PORT || 8000;
+
+
+
 
 // Tidigare kod, skillnaden är portnr
 app.listen(port, () => {
